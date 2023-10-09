84aa7235c7c42b3d6e5e1c85a2d71483ef9b510c
HusenZ <110398316+HusenZ@users.noreply.github.com>
2023-10-09 11:07:43 +0530

    Your commit message here

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:second_hand_mobils/constants/global_variables.dart';
+import 'package:second_hand_mobils/features/auth/auth_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      home: Scaffold(
+        body: AuthScreen(),
+      ),
+    );
+  }
+}
